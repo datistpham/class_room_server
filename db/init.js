3aae806be58acde7ca982118ae0da874f6ac6ce7
nhatnm3399 <nhatnm3399@gmail.com>
2022-11-16 15:29:06 +0700

    upload

--- db/init.js
+++ db/init.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose"
 
-const uri = process.env.URL_CONNECT_MONGO || "mongodb://localhost:27017/class_room";
+const uri = "mongodb://localhost:27017/class_room";
 const connectMongo= async ()=> {
   try {
-     mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true}, ()=> console.log("Mongoose is connected"))
+    mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true})
+    console.log("Connect mongo success")
   } catch (error) {
     console.log(error)
   }
