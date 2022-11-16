e52ad0e8dac0b451840e0402c74e6c73683ea698
nhatnm3399 <nhatnm3399@gmail.com>
2022-11-16 15:17:13 +0700

    upload

--- controller/marking_complete.js
+++ controller/marking_complete.js
@@ -1,7 +1,12 @@
 import { dbconnection } from "../db/init.js"
 
 const marking_complete= (req, res)=> {
-    dbconnection.collection("assignment").updateOne ({idAssignment: req.body.idAssignment, "attachment.userId" : req.body.userId}, {$set: {"attachment.$.score" :parseInt( req.body.score)}}, false )
+    dbconnection.collection("assignment").updateOne ({idAssignment: req.body.idAssignment, "attachment.userId" : req.body.userId}, {$set: {"attachment.$.score" :parseInt( req.body.score)}}, false, function(err, result) {
+        if(err) throw err
+        else {
+            return res.json({score: true})
+        }
+    } )
 }   
 
 export default marking_complete
