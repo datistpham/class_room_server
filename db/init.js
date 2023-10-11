134a0955c11a15af2f3cb41c4199d392d179227d
datistpham <78075406+datistpham@users.noreply.github.com>
2023-10-12 02:13:42 +0700

    Update init.js

--- db/init.js
+++ db/init.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const uri = "mongodb+srv://giang:giang@cluster0.pffyt.mongodb.net/class_room";
+const uri = "mongodb+srv://giang:giangvippro@cluster0.pffyt.mongodb.net/class_room";
 const connectMongo= async ()=> {
   try {
     mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true})
